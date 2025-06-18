b978859dc20ef3de25b0fb96f5c2b8bf47602ca1
karly <kalbarransanchez@danielcastelao.org>
2025-06-18 12:36:02 +0200

    constructor con parametros

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,6 +1,18 @@
+/**
+ * Clase calculadora que contendá los metodos
+ */
 public class Calculadora {
+    //atributos
     private double sumar;
     private double restar;
     private double multiplicar;
     private double dividir;
+
+    //constructor
+    public Calculadora (double sumar, double restar, double multiplicar, double dividir){
+        this.sumar = sumar;
+        this.restar = restar;
+        this.multiplicar = multiplicar;
+        this.dividir = dividir;
+    }
 }
