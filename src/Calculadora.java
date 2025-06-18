7bf9749d28fd149fb35747a3fa334a7c9024c724
karly <kalbarransanchez@danielcastelao.org>
2025-06-18 12:37:34 +0200

    getter y setters

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -16,3 +16,36 @@ public class Calculadora {
         this.dividir = dividir;
     }
 }
+//getters y setters
+    public double getSumar() {
+        return sumar;
+    }
+
+    public void setSumar(double sumar) {
+        this.sumar = sumar;
+    }
+
+    public double getRestar() {
+        return restar;
+    }
+
+    public void setRestar(double restar) {
+        this.restar = restar;
+    }
+
+    public double getMultiplicar() {
+        return multiplicar;
+    }
+
+    public void setMultiplicar(double multiplicar) {
+        this.multiplicar = multiplicar;
+    }
+
+    public double getDividir() {
+        return dividir;
+    }
+
+    public void setDividir(double dividir) {
+        this.dividir = dividir;
+    }
+}
