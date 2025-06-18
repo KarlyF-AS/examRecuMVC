e04b5d3e0dd43933db0b2ec4c639054a5d3b10a6
karly <kalbarransanchez@danielcastelao.org>
2025-06-18 12:41:16 +0200

    Metodos de sumar, restar, multiplicar y dividir

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -47,4 +47,20 @@ public class Calculadora {
     public void setDividir(double dividir) {
         this.dividir = dividir;
     }
+    //metodos
+    public double suma(double a, double b) {
+        return a + b;
+    }
+    public double resta(double a, double b) {
+        return a - b;
+    }
+    public double multiplicacion(double a, double b) {
+        return a * b;
+    }
+    public double division(double a, double b) {
+        if (b == 0) {
+            System.out.println("No se puede dividir por cero");
+        }
+        return a / b;
+    }
 }
